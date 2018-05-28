cc2389865121937d70b42265582a2b88f8d75c4d
arthurtruong <arthurtruong.work@gmail.com>
2018-05-28 15:45:29 +1000

    fixed problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
-  devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy'
-  end
 
   resources :publishers
   resources :games
